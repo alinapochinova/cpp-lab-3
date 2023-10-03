63e1cd7810a95747ec1b302a291696d616c5f6b6
alinapochinova <alinapochinova@mail.ru>
2023-10-03 11:26:45 +0300

    Update Source.cpp

--- 3/3/Source.cpp
+++ 3/3/Source.cpp
@@ -1,40 +1,27 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
 using namespace std;
-int distribution(vector <int> a, int n) {
-    int s1 = 0;
-    int s2 = 0;
-    for (int i = 0; i < n; i++) {
-        if (s1 >= s2) {
-            s2 = s2 + a[i];
-        }
-        else {
-            s1 = s1 + a[i];
-        }
-    }
-    return (abs(s1 - s2));
+long int min_difference = pow(10, 9);
+int n;
+long int a[32] = { 0 };
+int f(long int petya, long int masha, int i) {
+	if (i == n) {
+		if (abs(petya - masha) < min_difference) {
+			min_difference = abs(petya - masha);
+		}
+	}
+	else {
+		f(petya + a[i], masha, i + 1);
+		f(petya, masha + a[i], i + 1);
+	}
+	return 0;
 }
 int main() {
-    int n;
-    cin >> n;
-    vector <int> a;
-    for (int i = 0; i < n; i++) {
-        int temp;
-        cin >> temp;
-        a.push_back(temp);
-    }
-    for (int j = 0; j < n; j++) { 
-        int num_max = j; 
-        for (int i = j; i < n; i++) { 
-            if (a[i] > a[num_max]) {
-                num_max = i;
-            }
-        int temp;
-        temp = a[j];
-        a[j] = a[num_max];
-        a[num_max] = temp;
-    }
-    cout << distribution(a, n);
-    return 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	f(0, 0, 0);
+	cout << min_difference;
+	return 0;
 }
